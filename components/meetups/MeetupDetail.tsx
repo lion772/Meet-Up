dae3448ef296587650933a7fae7fef130b35fc9f
william <william.steinkedemello@gmail.com>
2022-11-27 18:50:12 +0100

    MeetupDrtail component and module created and meetup detail passed via props.

---
+++ components/meetups/MeetupDetail.tsx
@@ -0,0 +1,22 @@
+import styles from "./MeetupDetail.module.css";
+
+type DataDetail = {
+    id: string;
+    image: string;
+    address: string;
+    title: string;
+    description: string;
+};
+
+export default function MeetupDetail(props: DataDetail) {
+    console.log(props.image);
+    return (
+        <section className={styles.detail}>
+            <h1>Meetup Detail</h1>
+            <img src={props.image} alt={props.title} />
+            <address>{props.address}</address>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </section>
+    );
+}
--- pages/[meetupId]/index.tsx
+++ pages/[meetupId]/index.tsx
@@ -1,12 +1,18 @@
 import { useRouter } from "next/router";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-export default function MeetupDetail() {
+export default function MeetupDetailPage() {
     const { meetupId } = useRouter().query;
-    console.log(meetupId);
     return (
-        <>
-            <h1>Meetup Detail</h1>
-            <h1>{meetupId}</h1>
-        </>
+        <MeetupDetail
+            key={meetupId as string}
+            id={meetupId as string}
+            image={
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Panorama_of_Canal_Grande_and_Ponte_di_Rialto%2C_Venice_-_September_2017.jpg/1024px-Panorama_of_Canal_Grande_and_Ponte_di_Rialto%2C_Venice_-_September_2017.jpg"
+            }
+            address={"La Serenissima"}
+            title={"Venice"}
+            description={"This is a first meetup"}
+        />
     );
 }
