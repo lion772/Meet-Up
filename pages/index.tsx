e040eb56c8c361a2160b93186eb1d22766b9113b
william <william.steinkedemello@gmail.com>
2022-11-27 17:26:24 +0100

    Dummy data added and Meetups list outputted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import MeetupList from "../components/meetups/MeetupList";
 
-export default function MeetupList() {
+const DUMMY_MEETUPS = [
+    {
+        id: "m1",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Panorama_of_Canal_Grande_and_Ponte_di_Rialto%2C_Venice_-_September_2017.jpg/1024px-Panorama_of_Canal_Grande_and_Ponte_di_Rialto%2C_Venice_-_September_2017.jpg",
+        title: "Venice",
+        address: "La Serenissima",
+        description: "This is a first meetup",
+    },
+    {
+        id: "m2",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "Munich",
+        address: "Marienplatz",
+        description: "This is a second meetup",
+    },
+    {
+        id: "m3",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Chambord_pano.jpg/1024px-Chambord_pano.jpg",
+        title: "French",
+        address: "Chambord Pano",
+        description: "This is a third meetup",
+    },
+];
+
+export default function HomePage() {
     return (
         <>
-            <Head>
-                <h1>Hello</h1>
-            </Head>
+            <MeetupList meetups={DUMMY_MEETUPS} />
         </>
     );
 }
